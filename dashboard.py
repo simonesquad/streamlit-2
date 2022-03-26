48e3d3af4a3b1946b9eb4efae114ac76887046bf
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-26 16:55:29 -0500

    all data printing to the screen

--- dashboard.py
+++ dashboard.py
@@ -4,5 +4,20 @@ st.title("This is the title")
 
 st.header("This is a header")
 
+st.subheader("Subheader")
+
 st.write("this is regular text")
 
+"""
+# header
+## subheader
+"""
+
+some_dictionary = {
+    "key": "value",
+    "key2": "value2"
+}
+
+
+some_list = [1,2,3]
+st.write(some_dictionary)
